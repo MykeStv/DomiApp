086aa3a2242f53d00453d815bb0a947008e311b2
MykeStv <mykestv22@gmail.com>
2022-03-08 16:48:14 -0500

    Creando agregado orden preliminar

--- domi-app/src/main/java/orden/Orden.java
+++ domi-app/src/main/java/orden/Orden.java
@@ -1,12 +1,23 @@
 package orden;
 
 import co.com.sofka.domain.generic.AggregateEvent;
+import orden.entity.Factura;
+import orden.entity.Repartidor;
 import orden.values.OrdenId;
+import orden.values.Precio;
 
 public class Orden extends AggregateEvent<OrdenId> {
 
+    //TODO: conectar con los otros agregados
+    protected ClienteId clienteId;
+    protected PedidoId pedidoId;
+    protected CarritoId carritoId;
 
-    public Orden(OrdenId entityId) {
-        super(entityId);
+    protected Repartidor repartidor;
+    protected Factura factura;
+    protected Precio precio;
+
+    public Orden(OrdenId ordenId) {
+        super(ordenId);
     }
 }
--- domi-app/src/main/java/orden/values/Precio.java
+++ domi-app/src/main/java/orden/values/Precio.java
@@ -2,10 +2,22 @@ package orden.values;
 
 import co.com.sofka.domain.generic.ValueObject;
 
+import java.util.Objects;
+
 public class Precio implements ValueObject<Double> {
+
+    private final Double precio;
+
+    //CONSTRUCTOR
+    public Precio(Double precio) {
+        this.precio = Objects.requireNonNull(precio);
+        if (this.precio < 0) {
+            throw new IllegalArgumentException("No puedes tener un precio negativo");
+        }
+    }
 
     @Override
     public Double value() {
-        return null;
+        return precio;
     }
 }
