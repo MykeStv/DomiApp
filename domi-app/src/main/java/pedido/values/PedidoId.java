a8de2582d04147e3cd9eb6a1e282bdf6ff1ae9e0
jcelisca <celiss1997@gmail.com>
2022-03-08 16:42:21 -0500

    Comportamientos para la entidad estado

--- domi-app/src/main/java/pedido/values/PedidoId.java
+++ domi-app/src/main/java/pedido/values/PedidoId.java
@@ -3,5 +3,8 @@ package pedido.values;
 import co.com.sofka.domain.generic.Identity;
 
 public class PedidoId extends Identity{
-    
+
+    private PedidoId(String valor){
+        super(valor);
+    }
 }
